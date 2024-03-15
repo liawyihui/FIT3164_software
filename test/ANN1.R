43b7ec29b9957eea4a0bf74010666dcbf35b2506
liawyihui <ylia0072@student.monash.edu>
2024-03-15 16:10:51 +0800

    Fix ann

---
+++ test/ANN1.R
@@ -0,0 +1,69 @@
+setwd("D:/FIT3164_software/test")
+# loading the packages needed for the assignment/tasks
+library(dplyr)
+library(tidyr)
+library(skimr) # for summary statistics table
+library(scales)
+library(ggplot2)
+library(corrplot)
+library(rpart)
+library(caret) # confusion matrix
+library(tree) # for Decision Tree
+library(e1071) # for Naive Bayes and Support Vector Machines
+library(randomForest) # for Random Forest
+library(adabag) # for Bagging and Boosting
+library(neuralnet) # for ANN
+library(ROCR) # for AUC and ROC
+library(pROC)
+
+library(openxlsx)
+library(svDialogs)
+library(data.table)
+library(caret)
+library(dplyr)
+library(purrr)
+library(doMC)
+library(ROCit)
+
+# reading the csv file required and creating individual data by setting a seed (my Student ID)
+df <- read.csv("Lymph_dataset_raw.csv")
+
+Table1 <- df %>%
+  select(-c("id", "opd", "nam.y", "lnn","int", "le"))
+#select(-c("id", "opd", "nam.y", "tax", "lnn","axi","int", "che", "fx", "Gy", "recon", "le"))
+
+Table1$Endpoint <- factor(df$le)
+
+randomseed <- 1165# 365# 1675# 
+
+set.seed(randomseed)
+
+### ANN ###
+split_index <- createDataPartition(y = Table1$Endpoint, p = 0.8, list = FALSE)
+train_data <- Table1[split_index, ]
+test_data <- Table1[-split_index, ]
+
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+# train the model
+registerDoMC(cores=6)
+ann.model <- train(Endpoint~., data=train_data, method="pcaNNet", trControl=control, tuneLength=5)
+
+ann_predictions <- predict(ann.model, newdata = test_data, type="prob")
+
+ann_predictions_binary <- ifelse(ann_predictions[,1] > 0.5, 0, 1)
+
+# Calculate accuracy
+ann_accuracy <- mean(ann_predictions_binary == test_data$Endpoint)
+
+# Print the accuracy
+cat("ANN Accuracy:", ann_accuracy, "\n")
+
+# AUC, sensitivity and specificity
+#source("my.prediction.stats.R")
+#lymp_test <- factor(test_data$lymphedema, levels = c(0, 1))
+#my.pred.stats(tree_predictions_binary, lymp_test)
+
+confusionMatrix(table(actual = test_data$Endpoint, predicted = ann_predictions_binary))
+
+ROCit_obj_test <- rocit(score=ann_predictions_binary, class=test_data$Endpoint)
+ROCit_obj_test$AUC
---
+++ test/ANN2.R
@@ -0,0 +1,143 @@
+setwd("D:/FIT3164_software/test")
+# loading the packages needed for the assignment/tasks
+library(dplyr)
+library(tidyr)
+library(skimr) # for summary statistics table
+library(scales)
+library(ggplot2)
+library(corrplot)
+library(rpart)
+library(caret) # confusion matrix
+library(tree) # for Decision Tree
+library(e1071) # for Naive Bayes and Support Vector Machines
+library(randomForest) # for Random Forest
+library(adabag) # for Bagging and Boosting
+library(neuralnet) # for ANN
+library(ROCR) # for AUC and ROC
+library(pROC)
+
+library(openxlsx)
+library(svDialogs)
+library(data.table)
+library(caret)
+library(dplyr)
+library(purrr)
+library(doMC)
+library(ROCit)
+
+# reading the csv file required and creating individual data by setting a seed (my Student ID)
+DataTable <- read.csv("Lymph_dataset_raw.csv")
+
+Table1 <- df %>%
+  select(-c("id", "opd", "nam.y", "lnn","int", "le"))
+#select(-c("id", "opd", "nam.y", "tax", "lnn","axi","int", "che", "fx", "Gy", "recon", "le"))
+
+Table1$Endpoint <- factor(df$le)
+
+randomseed <- 1165# 365# 1675# 
+
+set.seed(randomseed)
+
+# Split data into train/test with ratio 8/2 of the sample size
+in_rows <- createDataPartition(y = Table1$Endpoint, p = 0.8, list = FALSE)
+train <- Table1[in_rows, ]
+test <- Table1[-in_rows, ]
+  
+  
+#-------------------------------------------------------------------------------
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+# train the model
+registerDoMC(cores=6)
+NNetmodel <- train(Endpoint~., data=train, method="pcaNNet", trControl=control, tuneLength=5)
+
+
+#-------------------------------------------------------------------------------
+# Find Cut-off value for probability to maximize balanced accuracy
+
+# Get probability
+pred_all_prob <- as.data.frame(NNetmodel %>% predict(Table1, type = "prob"))
+  
+Table_cutoff <- data.frame( "Cutoff"             = seq(0.01, 1, by= 0.01),
+                            "Balanced_Accuracy"  = 0)    
+for (i in (1:100)) {
+    pred.LE <- as.factor(ifelse(pred_all_prob$`1`>Table_cutoff$Cutoff[i],"1","0"))
+    Table2 <- table(factor(pred.LE, levels = c("0","1")), Table1$Endpoint)
+    ConfMat <- confusionMatrix(Table2)
+    Performance <- setDT(as.data.frame(ConfMat$byClass), keep.rownames = TRUE)[]
+    Table_cutoff$Balanced_Accuracy[i] <- Performance[11,2]
+}
+
+cutoff <- Table_cutoff$Cutoff[which.max(Table_cutoff$Balanced_Accuracy)]
+
+#-------------------------------------------------------------------------------
+# Make prediction on test set 
+pred_train_prob <- as.data.frame(NNetmodel %>% predict(train, type = "prob"))
+pred_test_prob <- as.data.frame(NNetmodel %>% predict(test, type = "prob"))
+pred_all_prob <- as.data.frame(NNetmodel %>% predict(Table1, type = "prob"))
+  
+pred_train <- as.factor(ifelse(pred_train_prob$`1`>cutoff,"1","0"))
+pred_test <- as.factor(ifelse(pred_test_prob$`1`>cutoff,"1","0"))
+pred_all <- as.factor(ifelse(pred_all_prob$`1`>cutoff,"1","0"))
+  
+  
+#-------------------------------------------------------------------------------
+# Confusion matrix and model performance
+union <- union(pred_all, Table1$Endpoint)
+Table2 <- table(factor(pred_all,union), factor(Table1$Endpoint,union))
+ConfMat <- confusionMatrix(Table2)
+Performance <- setDT(as.data.frame(ConfMat$byClass), keep.rownames = TRUE)[]
+colnames(Performance) <- c("Parameter", "Value")
+Performance
+mean(pred_all == Table1$Endpoint)
+  
+#-------------------------------------------------------------------------------
+# Confusion matrix and model performance in Train
+Table21 <- table(factor(pred_train,union), factor(train$Endpoint,union))
+ConfMat_train <- confusionMatrix(Table21)
+Performance_train <- setDT(as.data.frame(ConfMat_train$byClass), keep.rownames = TRUE)[]
+colnames(Performance_train) <- c("Parameter", "Value")
+Performance_train
+mean(pred_train == train$Endpoint)
+  
+#-------------------------------------------------------------------------------
+# Confusion matrix and model performance in Test
+Table22 <- table(factor(pred_test,union), factor(test$Endpoint,union))
+ConfMat_test <- confusionMatrix(Table22)
+Performance_test <- setDT(as.data.frame(ConfMat_test$byClass), keep.rownames = TRUE)[]
+colnames(Performance_test) <- c("Parameter", "Value")
+Performance_test
+mean(pred_test == test$Endpoint)
+
+
+#-------------------------------------------------------------------------------  
+# Estimate Descriptor importance
+DescImportance <- data.frame(Descriptor = row.names(varImp(NNetmodel, scale=TRUE)$importance),
+                            Value = varImp(NNetmodel, scale=TRUE)$importance$X0)
+colnames(DescImportance) <- c("Descriptor", "Value")
+DescImportance <- DescImportance[order(DescImportance$Value, decreasing = TRUE),]; DescImportance
+  
+# Combine original data (DataTable) with Probability
+Table3 <- DataTable
+Table3$Prediction <- pred_all
+Table3$Prediction_prob_0 <- pred_all_prob$`0`
+Table3$Prediction_prob_1 <- pred_all_prob$`1`
+  
+# Add Train/Test label to the combined data
+Table3$Train_Test <- ifelse(do.call(paste0, Table1) %in% do.call(paste0, train) == TRUE, "Train", "Test")
+  
+
+  
+#-------------------------------------------------------------------------------     
+# ROC and AUC data  
+ROCit_obj_all <- rocit(score=Table3$Prediction_prob_1,class=Table3$le); plot(ROCit_obj_all)
+ROCit_obj_test <- rocit(score=pred_test_prob$`1`,class=test$Endpoint); plot(ROCit_obj_test)
+ROCit_obj_train <- rocit(score=pred_train_prob$`1`,class=train$Endpoint); plot(ROCit_obj_train)
+  
+ROC_data_all <- data.frame(FPR = ROCit_obj_all$FPR,
+                            TPR = ROCit_obj_all$TPR)
+ROC_data_train <- data.frame(FPR = ROCit_obj_train$FPR,
+                            TPR = ROCit_obj_train$TPR)
+ROC_data_test <- data.frame(FPR = ROCit_obj_test$FPR,
+                            TPR = ROCit_obj_test$TPR)
+ROCit_obj_train$AUC
+ROCit_obj_test$AUC
