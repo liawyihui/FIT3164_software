b98f1e5e373d8ad7f880e7c1e880e01270d6d11c
liawyihui <ylia0072@student.monash.edu>
2024-03-24 13:06:15 +0800

    RUSBoost

---
+++ ensemble_learning/RUSBoost.R
@@ -0,0 +1,63 @@
+setwd("D:/FIT3164_software/ensemble_learning")
+# loading the packages needed for the assignment/tasks
+library(dplyr)
+library(tidyr)
+library(skimr) # for summary statistics table
+library(scales)
+library(ggplot2)
+library(corrplot)
+library(rpart)
+library(caret) # confusion matrix
+library(tree) # for Decision Tree
+library(e1071) # for Naive Bayes and Support Vector Machines
+library(randomForest) # for Random Forest
+library(adabag) # for Bagging and Boosting
+library(neuralnet) # for ANN
+library(ROCR) # for AUC and ROC
+library(pROC)
+
+library(openxlsx)
+library(svDialogs)
+library(data.table)
+library(caret)
+library(dplyr)
+library(purrr)
+library(doMC)
+library(ROCit)
+library(caretEnsemble)
+library(ebmc)
+
+# reading the csv file required and creating individual data by setting a seed (my Student ID)
+df <- read.csv("Lymph_dataset_raw.csv")
+
+Table1 <- df %>%
+  select(-c("id", "opd", "nam.y","int", "le"))
+#select(-c("id", "opd", "nam.y", "tax", "lnn","axi","int", "che", "fx", "Gy", "recon", "le"))
+
+Table1$Endpoint <- factor(df$le)
+
+# Exclude the Endpoint variable before normalizing
+independent_variables <- setdiff(names(Table1), "Endpoint")
+
+# Normalize independent variables
+Table1[, independent_variables] <- scale(Table1[, independent_variables])
+
+randomseed <- 1165# 365# 1675# 
+
+set.seed(randomseed)
+
+split_index <- createDataPartition(y = Table1$Endpoint, p = 0.8, list = FALSE)
+train_data <- Table1[split_index, ]
+test_data <- Table1[-split_index, ]
+
+model <- rus(Endpoint~., train_data, size=20, alg = "rf", ir = 1, rf.ntree = 50, svm.ker = "radial")
+
+rus_predictions <- predict(model , test_data, type="prob")
+rus_predictions_binary <- ifelse(rus_predictions > 0.5, 1, 0)
+accuracy <- mean(rus_predictions_binary == test_data$Endpoint)
+cat("RUSBoost Accuracy:", accuracy, "\n")
+
+confusionMatrix(factor(rus_predictions_binary), test_data$Endpoint, positive = "1")
+
+ROCit_obj_test <- rocit(score=rus_predictions, class=test_data$Endpoint)
+ROCit_obj_test$AUC
