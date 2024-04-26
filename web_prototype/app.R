b95784a84348aa93ac3d1bbcf64d0163a5846285
Jia Xin Chan <jcha0157@student.monash.edu>
2024-04-26 12:16:44 +0800

    Added results and ROC curve to webpage

--- web_prototype/app.R
+++ web_prototype/app.R
@@ -1,6 +1,8 @@
 # Load R packages
 library(shiny)
 library(shinythemes)
+library(readxl)
+library(ggplot2)
 
 
   # Define UI
@@ -25,8 +27,24 @@ library(shinythemes)
                ) # mainPanel
                
       ), # Navbar 1, tabPanel
-      tabPanel("About Model", "This panel is intentionally left blank")
-  
+      tabPanel("About Model",
+               fluidRow(
+                 column(12, tags$h3("Model Performance"))
+               ),
+               fluidRow(
+                 column(12, tableOutput("table"))
+               ),
+               fluidRow(
+                 column(4, tags$h3("ROC Curve")),
+                 column(4, tags$h3("____")),
+                 column(4, tags$h3("____"))
+               ),
+               fluidRow(
+                 column(4, plotOutput("ROC", height = "400px")),
+                 column(4, tags$h3("")),
+                 column(4, tags$h3(""))
+               )
+      ) # Navbar 2, tabPanel
     ) # navbarPage
   ) # fluidPage
 
@@ -36,6 +54,23 @@ library(shinythemes)
     
     output$txtout <- renderText({
       paste( input$txt1, input$txt2, sep = " " )
+    })
+
+    output$table <- renderTable({
+      performance_test <- read_xlsx("performance_test.xlsx")
+      
+      performance_test
+    })
+    
+    output$ROC <- renderPlot({
+      ROC_test <- read_xlsx("ROC_test. xlsx")
+
+      p2 <- ggplot() +
+      geom_line(data=ROC_test, aes(x=FPR, y=TPR), color="red") +
+      theme(text=element_text(size=16,  face="bold")) +
+      theme(axis.text.x = element_text(angle = 0, hjust = 1, colour = "black"))
+      
+      p2
     })
   } # server
   
