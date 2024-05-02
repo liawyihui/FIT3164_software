881bbcd17cf60e5391677e2432a2d9474372cd3d
liawyihui <ylia0072@student.monash.edu>
2024-05-02 14:04:15 +0800

    Added normalization

--- web_prototype/app.R
+++ web_prototype/app.R
@@ -32,52 +32,70 @@ ui <- fluidPage(
     "Lymphedema Prediction",
     tabsetPanel(
       id = "switcher",
-      tabPanel("Home",
-                 fluidRow(
-                   column(12, tags$h2("About Lymphedema"))
-                 ),
-                 fluidRow(
-                   column(6, p(style = "font-size: 17px; text-align: justify",
-                                "Lymphedema is a common condition that affects many breast cancer survivors.
-                                Lymphedema in breast cancer patients is swelling that occurs typically in one of the   
+      tabPanel(
+        "Home",
+        fluidRow(
+          column(12, tags$h2("About Lymphedema"))
+        ),
+        fluidRow(
+          column(
+            6, p(
+              style = "font-size: 17px; text-align: justify",
+              "Lymphedema is a common condition that affects many breast cancer survivors.
+                                Lymphedema in breast cancer patients is swelling that occurs typically in one of the
                                 arms, often as a result of cancer treatment such as surgery or radiation therapy
-                                which disrupts the drainage of lymph fluid by damaging or removing lymph nodes."),
-                             p(style = "font-size: 17px; text-align: justify",
-                                "While there is currently no known cure for lymphedema, various treatments and 
-                                strategies can help reduce symptoms, manage swelling, and improve quality of life for 
-                                patients. These may include compression therapy, physical therapy, exercise, skin care, 
-                                and in some cases, surgery. Early detection can also play a crucial role in 
-                                preventing the progression of lymphedema and minimizing its impact on 
-                                patients' lives.")),
-                   column(3, imageOutput("lymphedema_img")),
-                   column(3, imageOutput("lymphedema2_img"))
-                 ),
-                 fluidRow(
-                   column(12, tags$h2("3 Steps to Use Our Tool"))
-                 ),
-                 fluidRow(
-                   column(1, imageOutput("number1_img", height = "240px")),
-                   column(3, tags$h2("Input your Dataset"), 
-                             p(style = "font-size: 17px; text-align: justify",
-                                "Download the dataset template and fill in all necessary details for each patient.
-                                 Ensure completeness and accuracy of the data.")),
-                   column(1, imageOutput("number2_img", height = "240px")),
-                   column(3, tags$h2("Lymphedema Assessment"),
-                             p(style = "font-size: 17px; text-align: justify",
-                                "We evaluate the risk of lymphedema for each patient based on the dataset provided.")),
-                   column(1, imageOutput("number3_img", height = "240px")),
-                   column(3, tags$h2("Your Results"),
-                             p(style = "font-size: 17px; text-align: justify",
-                                "Results of the lymphedema risk assessment will be generated for each patient. 
-                                 These results will include individual risk scores or probabilities."))
-                 ),
-                 fluidRow(
-                   column(4, ""),
-                   column(4, actionButton("start_assess", "Start Assessment", style="font-size: 17px; background-color: #337ab7; border-color: #2e6da4; width: 100%")),
-                   column(4, "")
-                 )
-                 
-        ), # Navbar 1, tabPanel
+                                which disrupts the drainage of lymph fluid by damaging or removing lymph nodes."
+            ),
+            p(
+              style = "font-size: 17px; text-align: justify",
+              "While there is currently no known cure for lymphedema, various treatments and
+                                strategies can help reduce symptoms, manage swelling, and improve quality of life for
+                                patients. These may include compression therapy, physical therapy, exercise, skin care,
+                                and in some cases, surgery. Early detection can also play a crucial role in
+                                preventing the progression of lymphedema and minimizing its impact on
+                                patients' lives."
+            )
+          ),
+          column(3, imageOutput("lymphedema_img")),
+          column(3, imageOutput("lymphedema2_img"))
+        ),
+        fluidRow(
+          column(12, tags$h2("3 Steps to Use Our Tool"))
+        ),
+        fluidRow(
+          column(1, imageOutput("number1_img", height = "240px")),
+          column(
+            3, tags$h2("Input your Dataset"),
+            p(
+              style = "font-size: 17px; text-align: justify",
+              "Download the dataset template and fill in all necessary details for each patient.
+                                 Ensure completeness and accuracy of the data."
+            )
+          ),
+          column(1, imageOutput("number2_img", height = "240px")),
+          column(
+            3, tags$h2("Lymphedema Assessment"),
+            p(
+              style = "font-size: 17px; text-align: justify",
+              "We evaluate the risk of lymphedema for each patient based on the dataset provided."
+            )
+          ),
+          column(1, imageOutput("number3_img", height = "240px")),
+          column(
+            3, tags$h2("Your Results"),
+            p(
+              style = "font-size: 17px; text-align: justify",
+              "Results of the lymphedema risk assessment will be generated for each patient.
+                                 These results will include individual risk scores or probabilities."
+            )
+          )
+        ),
+        fluidRow(
+          column(4, ""),
+          column(4, actionButton("start_assess", "Start Assessment", style = "font-size: 17px; background-color: #337ab7; border-color: #2e6da4; width: 100%")),
+          column(4, "")
+        )
+      ), # Navbar 1, tabPanel
       tabPanel(
         "Prediction",
         fluidRow(
@@ -166,33 +184,58 @@ ui <- fluidPage(
 
 # define server function
 server <- function(input, output) {
-  output$lymphedema_img <- renderImage({
-      list(src = "img/Lymphedema-early-detection.jpg",
-           width = "100%",
-           height = 380)
-  }, deleteFile = FALSE)
-    
-  output$lymphedema2_img <- renderImage({
-    list(src = "img/10434_2014_3518_Fig1_HTML.jpg",
+  output$lymphedema_img <- renderImage(
+    {
+      list(
+        src = "img/Lymphedema-early-detection.jpg",
         width = "100%",
-        height = 380)
-  }, deleteFile = FALSE)
-    
-  output$number1_img <- renderImage({
-    list(src = "img/number1.png",
-          width = "100%")
-  }, deleteFile = FALSE)
-  
-  output$number2_img <- renderImage({
-    list(src = "img/number2.png",
-          width = "100%")
-  }, deleteFile = FALSE)
-  
-  output$number3_img <- renderImage({
-    list(src = "img/number3.png",
-          width = "100%")
-  }, deleteFile = FALSE)
-  
+        height = 380
+      )
+    },
+    deleteFile = FALSE
+  )
+
+  output$lymphedema2_img <- renderImage(
+    {
+      list(
+        src = "img/10434_2014_3518_Fig1_HTML.jpg",
+        width = "100%",
+        height = 380
+      )
+    },
+    deleteFile = FALSE
+  )
+
+  output$number1_img <- renderImage(
+    {
+      list(
+        src = "img/number1.png",
+        width = "100%"
+      )
+    },
+    deleteFile = FALSE
+  )
+
+  output$number2_img <- renderImage(
+    {
+      list(
+        src = "img/number2.png",
+        width = "100%"
+      )
+    },
+    deleteFile = FALSE
+  )
+
+  output$number3_img <- renderImage(
+    {
+      list(
+        src = "img/number3.png",
+        width = "100%"
+      )
+    },
+    deleteFile = FALSE
+  )
+
   observeEvent(input$start_assess, {
     updateTabsetPanel(inputId = "switcher", selected = "Prediction")
   })
@@ -227,7 +270,13 @@ server <- function(input, output) {
 
       inFile <- input$DataFile
       DataTable <- read.csv(inFile$datapath)
-      Pred.prob <- predict(model, DataTable, type = "prob")
+
+      Normalized_DataTable <- DataTable
+      # Exclude the Endpoint variable before normalizing
+      independent_variables <- setdiff(names(Normalized_DataTable), c("Endpoint", "ID"))
+      Normalized_DataTable[, independent_variables] <- scale(Normalized_DataTable[, independent_variables])
+
+      Pred.prob <- predict(model, Normalized_DataTable, type = "prob")
       OutputTable <- data.frame(
         Patient.ID = as.factor(DataTable$ID),
         Predicted.Score = round(Pred.prob, 3),
@@ -237,8 +286,6 @@ server <- function(input, output) {
     selection = "single"
   )
 
-
-
   # predicting lymphedema for the selected patient
   output$pred.single <- renderValueBox({
     validate(need(input$DataFile, "Missing data file!"))
@@ -247,7 +294,12 @@ server <- function(input, output) {
     DataTable <- read.csv(inFile$datapath)
     DataTable$Patient.ID <- as.character(DataTable$ID)
 
-    Pred.prob <- predict(model, DataTable, type = "prob")
+    Normalized_DataTable <- DataTable
+    # Exclude the Endpoint variable before normalizing
+    independent_variables <- setdiff(names(Normalized_DataTable), c("Patient.ID", "ID", "Endpoint"))
+    Normalized_DataTable[, independent_variables] <- scale(Normalized_DataTable[, independent_variables])
+
+    Pred.prob <- predict(model, Normalized_DataTable, type = "prob")
     Pred.Table <- data.frame(
       Patient.ID = as.factor(DataTable$ID),
       Predicted.Score = round(Pred.prob, 3),
@@ -383,25 +435,27 @@ server <- function(input, output) {
   })
 
   output$variable_impt <- renderPlot({
-      variable_impt <- read_xlsx("variance_importance.xlsx")
-      
-      p1 <- ggplot(variable_impt, aes(x = reorder(variable, +score), y=score)) + 
-      geom_bar(stat="identity", fill = "skyblue2") +
+    variable_impt <- read_xlsx("variance_importance.xlsx")
+
+    p1 <- ggplot(variable_impt, aes(x = reorder(variable, +score), y = score)) +
+      geom_bar(stat = "identity", fill = "skyblue2") +
       geom_text(aes(label = round(score, 1)), position = position_stack(vjust = 0.5), size = 3, colour = "black") +
       labs(x = "", y = "Relative Importance") +
-      theme(text=element_text(size=14, face="bold"),
-            axis.text.x = element_text(angle = 0, hjust = 1, colour = "black")) +
+      theme(
+        text = element_text(size = 14, face = "bold"),
+        axis.text.x = element_text(angle = 0, hjust = 1, colour = "black")
+      ) +
       coord_flip() # Horizontal bar plot
-      
-      p1 
-    })
+
+    p1
+  })
 
   output$ROC <- renderPlot({
     ROC_test <- read_xlsx("ROC_test.xlsx")
 
     p2 <- ggplot() +
       geom_line(data = ROC_test, aes(x = FPR, y = TPR), color = "blue") +
-      geom_abline(slope=1, intercept=0, color="red", linetype=2) +
+      geom_abline(slope = 1, intercept = 0, color = "red", linetype = 2) +
       theme(text = element_text(size = 16, face = "bold")) +
       theme(axis.text.x = element_text(angle = 0, hjust = 1, colour = "black"))
 
